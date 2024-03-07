3c561f8546d3af55ef28e422c9f929241b882b70
harishSatnami <cs23m025@smail.iitm.ac.in>
2024-03-08 01:45:31 +0530

    Added functions to calculate derivative

---
+++ feedforwardneuralnetwork/derivatives.py
@@ -0,0 +1,10 @@
+from activation_functions import sigmoid, relu, tanh
+
+def derivative_sigmoid(a):
+    return sigmoid(a) * (1-sigmoid(a))
+
+def derivative_tanh(a):
+    return 1 - (tanh(a)**2)
+
+def derivative_relu(a):
+    pass
