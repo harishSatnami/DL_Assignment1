e23721e6a5279738a69a6992d4e8a92f3d96f022
harishSatnami <cs23m025@smail.iitm.ac.in>
2024-03-08 01:48:24 +0530

    initial implementation of backward_propagation

--- feedforwardneuralnetwork/backward_propagate.py
+++ feedforwardneuralnetwork/backward_propagate.py
@@ -1,4 +1,30 @@
+import numpy as np
+from utility import hadamard_product
+
 # Backward Propagation
 
-def backward_propagation(H, A, y_actual, y_pred):
-    pass
+def backward_propagation(H, A, W, y_actual, y_pred, number_of_layers):
+    delta_A = [0 for i in range(number_of_layers)]
+    delta_W = [0 for i in range(number_of_layers)]
+    delta_B = [0 for i in range(number_of_layers)]
+    delta_H = [0 for i in range(number_of_layers-1)]
+
+    # gradient with respect to output
+    delta_A[-1] = -(y_actual-y_pred)
+
+    for k in reversed(range(number_of_layers)):
+
+        # gradient with respect to parameters
+        delta_W[k] = np.outer(delta_A[k],H[k-1])
+        delta_B[k] = delta_A[k]
+
+        if k==0:
+            break
+        # gradient with respect to layer below
+        delta_H[k-1] = np.matmul(W[k].tranpose() , delta_A[k])
+
+        #gradient with respect to layer below (i.e. pre-activation)
+        delta_A[k-1] = hadamard_product(delta_H[k-1],[derivative(i) for i in A[k-1]])
+        
+
+    return delta_W, delta_B
