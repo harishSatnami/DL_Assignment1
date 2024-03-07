4c731b86eb9a86495e0e6bb623ef9a2498f6329a
harishSatnami <cs23m025@smail.iitm.ac.in>
2024-03-08 01:47:24 +0530

    updated parameters in function forward_propagation

--- feedforwardneuralnetwork/forward_propagate.py
+++ feedforwardneuralnetwork/forward_propagate.py
@@ -5,14 +5,14 @@ from activation_functions import sigmoid_vector, relu_vector, tanh_vector, softm
 
 activation = sigmoid_vector
 
-def forward_propagation(input, Weights_w, Weights_l, Biases_b, Biases_l, number_of_layers):
+def forward_propagation(input, Weights, Biases, number_of_layers):
     A = []
     H = [input]
-    for i in range(1,number_of_layers):
-        A.append(pre_activation(Weights_w[i],H[i-1],Biases_b[i]))
-        H.append(activation(A[i-1]))
+    for i in range(number_of_layers-1):
+        A.append(pre_activation(Weights[i],H[i],Biases[i]))
+        H.append(activation(A[i]))
 
-    A.append(pre_activation(Weights_l, H[-1], Biases_l))
+    A.append(pre_activation(Weights[-1], H[-1], Biases[-1]))
 
     y_pred = softmax(A[-1])
 
