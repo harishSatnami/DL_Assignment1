37bc5791de23d5e3c27730d17b538c4d05960b6a
harishSatnami <cs23m025@smail.iitm.ac.in>
2024-03-08 01:46:37 +0530

    Added function to initialize the weights and biases

--- feedforwardneuralnetwork/utility.py
+++ feedforwardneuralnetwork/utility.py
@@ -9,3 +9,12 @@ def hadamard_product(A,B):
         result.append(A[i]*B[i])
 
     return result
+
+def random_initialize(number_of_layers, nodes_per_hidden_layer, nodes_in_output_layer):
+    W = np.random.rand(number_of_layers-1, nodes_per_hidden_layer ,nodes_per_hidden_layer)
+    B = np.random.rand(number_of_layers-1, nodes_per_hidden_layer)
+    WL = np.random.rand(nodes_per_hidden_layer,nodes_in_output_layer)
+    BL = np.random.rand(nodes_in_output_layer)
+    W_list = list(W).append(WL)
+    B_list = list(B).append(BL)
+    return W_list, B_list
