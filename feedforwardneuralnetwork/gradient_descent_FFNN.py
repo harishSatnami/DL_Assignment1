d26b3707992a9c146b1ad593ec63e8259293248c
harishSatnami <cs23m025@smail.iitm.ac.in>
2024-03-08 01:49:12 +0530

    Initial implementation for gradient_descent

---
+++ feedforwardneuralnetwork/gradient_descent_FFNN.py
@@ -0,0 +1,22 @@
+from utility import random_initialize
+from forward_propagate import forward_propagation
+from backward_propagate import backward_propagation
+
+def update_weights_and_biases(learning_rate, Weights, Biases, delta_Weights, delta_Biases):
+    for i in range(len(Weights)):
+        Weights[i] = Weights[i] - learning_rate * delta_Weights[i]
+        Biases[i] = Biases[i] - learning_rate * delta_Biases[i]
+
+    return Weights, Biases
+
+
+
+def gradient_descent(input, learning_rate, number_of_layers, max_iteration, nodes_per_hidden_layer, nodes_in_output_layer):
+    Weights, Biases = random_initialize(number_of_layers,nodes_per_hidden_layer,nodes_in_output_layer)
+    itr = 0
+    while itr<max_iteration:
+        H, A, y_pred = forward_propagation(input, Weights, Biases, number_of_layers)
+        delta_Weights, delta_Biases = backward_propagation(H, A, Weights, y_actual, y_pred, number_of_layers)
+        Weights , Biases = update_weights_and_biases(learning_rate, Weights, Biases, delta_Weights, delta_Biases)
+    
+    return Weights, Biases
