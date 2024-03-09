dda5a8349cebbc3f3c01df9d6fe0fb52811d71b7
harishSatnami <cs23m025@smail.iitm.ac.in>
2024-03-09 17:34:39 +0530

    Upadte to handle the parameters

--- feedforwardneuralnetwork/gradient_descent_FFNN.py
+++ feedforwardneuralnetwork/gradient_descent_FFNN.py
@@ -11,12 +11,12 @@ def update_weights_and_biases(learning_rate, Weights, Biases, delta_Weights, del
 
 
 
-def gradient_descent(input, learning_rate, number_of_layers, max_iteration, nodes_per_hidden_layer, nodes_in_output_layer):
-    Weights, Biases = random_initialize(number_of_layers,nodes_per_hidden_layer,nodes_in_output_layer)
+def gradient_descent(X, Y, learning_rate, number_of_layers, number_of_batch, batch_size, nodes_per_hidden_layer, nodes_in_output_layer, Weights, Biases):
+    # Weights, Biases = random_initialize(number_of_layers,nodes_per_hidden_layer,nodes_in_output_layer)
     itr = 0
-    while itr<max_iteration:
-        H, A, y_pred = forward_propagation(input, Weights, Biases, number_of_layers)
-        delta_Weights, delta_Biases = backward_propagation(H, A, Weights, y_actual, y_pred, number_of_layers)
+    while itr<number_of_batch:
+        H, A, y_pred = forward_propagation(X[itr*batch_size:(itr+1)*batch_size], Weights, Biases, number_of_layers)
+        delta_Weights, delta_Biases = backward_propagation(H, A, Weights, Y[itr*batch_size:(itr+1)*batch_size], y_pred, number_of_layers)
         Weights , Biases = update_weights_and_biases(learning_rate, Weights, Biases, delta_Weights, delta_Biases)
     
     return Weights, Biases
