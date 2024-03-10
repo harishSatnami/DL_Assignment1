9ce31ef2243c20a55ed5f954e1057075c8373572
harishSatnami <cs23m025@smail.iitm.ac.in>
2024-03-11 04:57:41 +0530

    updated for error free execution

--- feedforwardneuralnetwork/training.py
+++ feedforwardneuralnetwork/training.py
@@ -5,7 +5,9 @@ import math
 
 from activation_functions import sigmoid_vector, relu_vector, tanh_vector
 from utility import random_initialize
-from gradient_descent_FFNN import gradient_descent
+from gradient_descent_FFNN import gradient_descent , gradient_descent_mini_batch
+
+
 
 def train_model(X, Y, epochs=1, num_of_hidden_layers=1, size_of_layers=4, learning_rate=0.1, optimizer="sgd", batch_size=4, weight_init_type=random_initialize, activation_function=sigmoid_vector):
 
@@ -13,8 +15,12 @@ def train_model(X, Y, epochs=1, num_of_hidden_layers=1, size_of_layers=4, learni
     num_of_batchs = math.ceil(num_of_datapoints / batch_size)
     Weights, Biases = weight_init_type(num_of_hidden_layers+2,size_of_layers,Y.shape[1], X.shape[1])
 
+    if optimizer=="mini_batch":
+        gradient = gradient_descent_mini_batch
+    else:
+        gradient = gradient_descent
     for epoch in range(epochs):
         print("Epoch number", epoch+1, " started")
-        Weights, Biases = gradient_descent(X,Y,learning_rate,num_of_hidden_layers+2,num_of_batchs,batch_size,size_of_layers,Y.shape[1],Weights,Biases)
-        print("Epoch ",epoch, " finished.")
+        Weights, Biases = gradient(X,Y,learning_rate,num_of_hidden_layers+2,num_of_batchs,batch_size,size_of_layers,Y.shape[1],Weights,Biases)
+        print("Epoch ",epoch+1, " finished.")
     return Weights, Biases
