deeb59f96ad2e7f5d09bb9f14ecfc60df7e66a6f
harishSatnami <cs23m025@smail.iitm.ac.in>
2024-03-11 04:55:41 +0530

    updated parameters

--- feedforwardneuralnetwork/testing.py
+++ feedforwardneuralnetwork/testing.py
@@ -2,7 +2,7 @@ from forward_propagate import forward_propagation
 
 def validate(X, Weights, Biases):
     # some calculations
-    H, A, Y_pred = forward_propagation(X, Weights=Weights, Biases=Biases, number_of_layers=len(Weights))
+    H, A, Y_pred = forward_propagation(X, Weights=Weights, Biases=Biases, number_of_layers=len(Weights)+1)
     return Y_pred
 
 
